34044866e04afca2ad5f09f5064ec92e755f6e70
Pikoulis <71182326+Pikoulis@users.noreply.github.com>
2020-09-18 11:25:07 +0300

    idedu daugiau gal bus aisku kas blogai

---
+++ NamuDarbai_4/Page/DropdownDemoPage.cs
@@ -0,0 +1,83 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace NamuDarbai_4.Page
+{
+    class DropdownDemoPage : BasePage
+    {
+        private const string PageAddress = "https://www.seleniumeasy.com/test/basic-select-dropdown-demo.html";
+        private IWebElement Responce => Driver.FindElement(By.ClassName("getall-selected"));
+        private IWebElement FirstResultButton => Driver.FindElement(By.Id("printMe"));
+        private IWebElement AllResultsButton => Driver.FindElement(By.Id("printAll"));
+        private SelectElement MultipleDropdown => new SelectElement(Driver.FindElement(By.Id("multi-select")));
+
+
+        public DropdownDemoPage(IWebDriver webdriver) : base(webdriver)
+        {
+            Driver.Url = PageAddress;
+        }
+        public DropdownDemoPage SelectFromMultipleDropDownByValue(List<string> value)
+        {
+            if (value.Count() > 1)
+            {
+                Actions act = new Actions(Driver); //visiskai nesuveikia neaisku kodel
+                act.KeyDown(Keys.Control).B‌​uild().Perform();
+                foreach (string element in value)
+                {
+                    
+                    MultipleDropdown.SelectByValue(element);
+                    
+                }
+                act.KeyUp(Keys.Control).B‌​uild().Perform();
+            }
+            else
+            {
+                MultipleDropdown.SelectByValue(value[0]);
+            }
+            return this;
+        }
+        public DropdownDemoPage CheckFirstSelectedResult(List<string> value)
+        {
+            FirstResultButton.Click();
+            List<string> onevalue = new List<string>
+                { value[0] };
+            string stateValue = FromListToString(onevalue);
+            string fullText = "First selected option is : " + stateValue;
+            Assert.AreEqual(fullText, Responce.Text, $"Tekstai nesutampa, nerodo {fullText}");
+
+            return this;
+        }
+        public DropdownDemoPage CheckAlSelectedResult(List<string> value)
+        {
+            AllResultsButton.Click();
+            string stateValue = FromListToString(value);
+            string fullText = "Options selected are : " + stateValue;
+            Assert.AreEqual(fullText, Responce.Text, $"Tekstai nesutampa, nerodo {fullText}");
+
+            return this;
+        }
+        private string FromListToString(List<string> value)
+        {
+            string stateValue = "";
+            int i = 0;
+            foreach (string element in value)
+            { 
+                if(i == 0) 
+                {
+                    stateValue +=  element.ToString();
+                }
+                else 
+                {
+                    stateValue += " " + element.ToString();
+                }
+                i++;
+            }
+            return stateValue;
+        }
+    }
+}
---
+++ NamuDarbai_4/Test/DropDownTest.cs
@@ -0,0 +1,52 @@
+﻿
+using NamuDarbai_4.Page;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+
+namespace NamuDarbai_4.Test
+{
+    class DropDownTest
+    {
+        private static DropdownDemoPage _page;
+        [SetUp]
+        public static void SetUp()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            _page = new DropdownDemoPage(driver);
+        }
+        [Test]
+        public static void CheckFirstSelectedResult()
+        {
+            List<string> States = new List<string>
+            {
+                "California",
+                "New York",
+                "Texas"
+            };
+            _page.SelectFromMultipleDropDownByValue(States).CheckFirstSelectedResult(States);
+        }
+        [Test]
+        public static void CheckAlSelectedResult()
+        {
+            List<string> States = new List<string>
+            {
+                "California",
+                "New York",
+                "Texas",
+                "Ohio"
+            };
+            _page.SelectFromMultipleDropDownByValue(States).CheckAlSelectedResult(States);
+        }
+        [TearDown]
+        public static void TearDown()
+        {
+            _page.CloseBrowser();
+        }
+    }
+}
